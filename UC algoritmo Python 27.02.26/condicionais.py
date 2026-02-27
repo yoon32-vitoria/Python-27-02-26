1c43eaba084d6f8b3bfab1099a6d26dc9034fbf6
yoon32-vitoria <vitoria.reis.neko@gmail.com>
2026-02-27 16:10:12 -0300

    Add files via upload

---
+++ UC algoritmo Python 27.02.26/condicionais.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome completo: ")
+if len(nome) > 50:
+    print("Seu nome égrnado, tem mais de 50 caracteres. ")
+    print(f"Ele possui {len(nome)}caracteres. ")
---
+++ UC algoritmo Python 27.02.26/exemplo.py
@@ -0,0 +1,4 @@
+altura = float(input("Digite a sua altura:  "))
+altura = altura * 100
+
+print(f"Sua altura é de {altura} cm")
